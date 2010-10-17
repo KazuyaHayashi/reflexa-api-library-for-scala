95c1e4192f87f95ed150d6c3e12caaf563a33aac
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2010-10-18 01:09:54 +0900

    add source

---
+++ src/reflexa.scala
@@ -0,0 +1,55 @@
+import scala.io.Source
+import scala.xml.XML
+import scala.xml.Elem
+import scala.List
+
+import java.net.URLEncoder
+
+class Reflexa() {
+    val url = "http://labs.preferred.jp/reflexa/api.php"
+
+    /*
+     * reflexa apiへアクセス
+     */
+    def getXMLSource(query: String): Elem = {
+        val src = Source.fromURL(url + "?q=" + query + "&format=xml")
+        return XML.loadString(src.mkString)
+    }
+
+    /*
+     * 検索結果のxmlをパースし、結果の配列を作成
+     */
+    def makeWordList(query: String): Array[String] = {
+        val xml = getXMLSource(query)
+        val words = xml \\ "word"
+        words size match {
+            case 0 => Array()
+            case _ => words.map(word => word.text).toArray
+        }
+    }
+
+    /* 
+     * 検索文字列からqueryを作成
+     * 文字列同士は「%20」で連結
+     */
+    def makeQuery(search_words: Array[String]): String = {
+        return search_words.map(URLEncoder.encode).mkString("%20")
+    }
+    
+    def search(search_words: Array[String]): Array[String] = {
+        val query = makeQuery(search_words)
+        return makeWordList(query)
+    }
+}
+
+object reflexa {
+    def main(arg: Array[String]) = {
+        val Ref = new Reflexa()
+        val line = Console.readLine(">>> ")
+        val split_reg = """[\s　]""".r
+        val list = Ref.search(split_reg.split(line))
+        list.map(println)
+    }
+}
+
+// vim: set ts=4 sw=4 et:
